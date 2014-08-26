578d79a779e372b7382c8d486149a0637f4a3e9e
RobertoC27 <chi13027@uvg.edu.gt>
2014-08-25 19:26:00 -0600

    falta container solo
    ya hace todo lo que debería hacer a excepcion de manejar la memoria

--- hoja5.py
+++ hoja5.py
@@ -19,6 +19,7 @@ class Procesos(Process):
 		while(self.cantInstrucciones>0):
 			yield release,self,ready
 			print self.nombre, "está listo para ser atendido por el cpu"
+			
 			if (self.cantInstrucciones-i)<=0:
 				print "No se necesitan más de 1 ciclo para ",self.nombre , ", usa ",self.cantInstrucciones, "instrucciones" 
 				yield request, self, cpu
@@ -27,10 +28,8 @@ class Procesos(Process):
 				i=i+1
 				yield release, self, cpu
 				print self.nombre, "libera cpu"
-				
-				
-				
-				
+				print self.nombre, "sale del sistema"
+					
 			else:
 				print "Se necesitan más de 1 ciclo para el proceso",self.nombre, "usa ",self.cantInstrucciones
 				yield request, self, cpu
@@ -39,8 +38,8 @@ class Procesos(Process):
 				i=i+1
 				yield release, self, cpu
 				print self.nombre, "libera cpu"
-				decision=azar.randint(1,2+1)
-				if decision==1:
+				decision=random.randint(1,2+1)
+				if decision==1: #entra a la cola de waiting
 					yield request,self,waiting
 					print self.nombre, "está en espera para ser atendido por el cpu"
 					yield hold,self,1
@@ -49,16 +48,16 @@ class Procesos(Process):
 					yield request,self,ready
 					print self.nombre, "en cola para ser atendido por el cpu"
 				
-				else:
+				else: #regresa al estado de ready
 					yield request,self,ready
 					print self.nombre, "en cola para ser atendido por el cpu"
 				
 
 
 initialize() #inicar la simulacion. Tiempo = 0
-nrCars=1
+nrCars=2
 cpu = Resource(capacity=1)
-azar = Random(12345) #se puede reproducir los mismos numeros al azar
+azar = Random(87298673) #se puede reproducir los mismos numeros al azar
 parkingLot=Resource(capacity=4,qType=FIFO)
 #memoria = container(init=100,capacity=100)
 Memoria= Resource(capacity=100)
@@ -71,6 +70,6 @@ for i in range(nrCars):
     #activar los carros en la lista de simulacion. Se genera un numero
     #al azar para indicar el tiempo que se conduce cada carro para llegar
     #a la meta.
-    activate(c,c.ejecutar(cantRAM=azar.randint(1,10+1),cantInstrucciones=12))
+    activate(c,c.ejecutar(cantRAM=azar.randint(1,10+1),cantInstrucciones=azar.randint(1,10+1)))
     
 simulate(until=100) #simular para 100 unidades de tiempo
