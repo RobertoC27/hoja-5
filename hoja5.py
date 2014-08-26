bc81f2f24e9e62116b6042c95a1281e32cf27421
RobertoC27 <chi13027@uvg.edu.gt>
2014-08-25 19:13:31 -0600

    sin container de memoria pero ya usa cola para hacer procesos
    no hay container, usa colas despues de ejecutar cada proceso para saber
    si hay que esperar o ejecutar de nuevo

--- hoja5.py
+++ hoja5.py
@@ -9,17 +9,18 @@ class Procesos(Process):
 		if Memoria>=cantRAM:
 			self.RAM=cantRAM		
 			print "El proceso", self.nombre ,"solicita ", self.RAM ," MB de RAM"
-			yield request
+			
 		else:
 			print "No hay suficiente memoria para el proceso", self.nombre
 		self.cantInstrucciones=cantInstrucciones
 		i=3
 		yield request,self,ready
-		print self.nombre, "está listo para ser atendido por el cpu"
+		print self.nombre, "en cola para ser atendido por el cpu"
 		while(self.cantInstrucciones>0):
 			yield release,self,ready
+			print self.nombre, "está listo para ser atendido por el cpu"
 			if (self.cantInstrucciones-i)<=0:
-				print "No se necesitan más de 1 ciclo para el proceso",self.nombre , "usa ",self.cantInstrucciones, "instrucciones" 
+				print "No se necesitan más de 1 ciclo para ",self.nombre , ", usa ",self.cantInstrucciones, "instrucciones" 
 				yield request, self, cpu
 				print self.nombre, "obtiene cpu"
 				self.cantInstrucciones=self.cantInstrucciones-i
@@ -33,18 +34,24 @@ class Procesos(Process):
 			else:
 				print "Se necesitan más de 1 ciclo para el proceso",self.nombre, "usa ",self.cantInstrucciones
 				yield request, self, cpu
+				print self.nombre, "obtiene cpu"
 				self.cantInstrucciones=self.cantInstrucciones-i
 				i=i+1
 				yield release, self, cpu
+				print self.nombre, "libera cpu"
 				decision=azar.randint(1,2+1)
-					if decision==1:
-						yield request,self,waiting
-						yield hold,self,1
-						yield release,self,waiting
-					
-					else:
-						yield request,self,ready
-					
+				if decision==1:
+					yield request,self,waiting
+					print self.nombre, "está en espera para ser atendido por el cpu"
+					yield hold,self,1
+					print self.nombre, "sale de espera para ser atendido por el cpu"
+					yield release,self,waiting
+					yield request,self,ready
+					print self.nombre, "en cola para ser atendido por el cpu"
+				
+				else:
+					yield request,self,ready
+					print self.nombre, "en cola para ser atendido por el cpu"
 				
 
 
@@ -64,6 +71,6 @@ for i in range(nrCars):
     #activar los carros en la lista de simulacion. Se genera un numero
     #al azar para indicar el tiempo que se conduce cada carro para llegar
     #a la meta.
-    activate(c,c.ejecutar(cantRAM=azar.randint(1,10+1),cantInstrucciones=azar.randint(1,10+1)))
+    activate(c,c.ejecutar(cantRAM=azar.randint(1,10+1),cantInstrucciones=12))
     
 simulate(until=100) #simular para 100 unidades de tiempo
